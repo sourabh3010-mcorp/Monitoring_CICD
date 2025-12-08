094759d297477bb82e8e7ff73401980396822910
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-08 20:41:38 +0530

    code changes

--- FailureDetections/terraform/provider.tf
+++ FailureDetections/terraform/provider.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = ">= 1.7.5"
-
-  backend "azurerm" {
-    # DO NOT put values here
-    # They will be injected by Azure DevOps pipeline
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0"   # Example: latest at this writing
+    }
   }
+
+  required_version = ">= 1.7.5"
 }
 
 provider "azurerm" {
