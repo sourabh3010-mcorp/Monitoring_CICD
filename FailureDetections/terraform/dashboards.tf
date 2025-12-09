b5c5efd5986e2c2197553569103cd12bd4818de8
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-09 12:56:55 +0530

    code changes

--- FailureDetections/terraform/dashboards.tf
+++ FailureDetections/terraform/dashboards.tf
@@ -5,11 +5,10 @@ locals {
 resource "azapi_resource" "dashboards" {
   for_each = { for f in local.dashboards : basename(f) => f }
 
-  type      = "Microsoft.Portal/dashboards@2022-06-01-preview"
+  type      = "Microsoft.Portal/dashboards@2020-09-01-preview"
   name      = "${var.environment}-dashboard-${replace(each.key, ".json", "")}"
   parent_id = azurerm_resource_group.rg.id
 
-  schema_validation_enabled = false
 
   body = jsondecode(file("${path.module}/../dashboard/${each.value}"))
 }
