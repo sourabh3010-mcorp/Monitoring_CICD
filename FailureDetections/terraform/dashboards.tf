5511887200f77fbc5ecd0e5551eb5c486c3afa10
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-09 12:25:13 +0530

    code changes

--- FailureDetections/terraform/dashboards.tf
+++ FailureDetections/terraform/dashboards.tf
@@ -2,6 +2,8 @@ locals {
   dashboard_files = var.dashboard_files
 }
 
+
+
 resource "azurerm_resource_group_template_deployment" "dashboards" {
   for_each = { for f in local.dashboard_files : basename(f) => f }
 
@@ -9,9 +11,8 @@ resource "azurerm_resource_group_template_deployment" "dashboards" {
   resource_group_name = var.resource_group_name
   deployment_mode     = "Incremental"
 
-  template_content = file("${path.module}/../dashboard/${each.value}")
+  template_content = file(abspath("${path.module}/../dashboard/${each.value}"))
 
-  parameters_content = jsonencode({
-    dashboardName = "${var.environment}-dashboard-${replace(each.key, ".json", "")}"
-  })
+  parameters_content = jsonencode({})
 }
+
