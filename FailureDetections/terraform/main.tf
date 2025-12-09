63138785450196634c5eec76a0a4a103b2f6555d
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-09 10:18:05 +0530

    code changes

--- FailureDetections/terraform/main.tf
+++ FailureDetections/terraform/main.tf
@@ -2,21 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-
-locals {
-  workbook_files = var.workbook_files
-}
-
-resource "azurerm_resource_group_template_deployment" "workbooks" {
-  for_each = {
-    for f in local.workbook_files :
-    basename(f) => f
-  }
-
-  name                = "${var.environment}-workbook-${replace(each.key, ".json", "")}"
-  resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-
-  template_content = file("${path.module}/../workbooks/${each.value}")
-  parameters_content = jsonencode({})
-}
