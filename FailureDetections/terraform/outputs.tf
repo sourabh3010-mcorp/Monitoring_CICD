cf018bbcfc19b0b8d9069b8004c5b4baf5776ec2
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-09 12:39:59 +0530

    code changes

--- FailureDetections/terraform/outputs.tf
+++ FailureDetections/terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "workbooks_deployed" {
   value = keys(azurerm_resource_group_template_deployment.workbooks)
 }
-
-output "dashboards_deployed" {
-  value = keys(azurerm_resource_group_template_deployment.dashboards)
-}
