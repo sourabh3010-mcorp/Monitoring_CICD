78f04befe7aa29a7fcb142570495ba35a1914bf0
sourabh3010-mcorp <sourabh.chakraborty@globant.com>
2025-12-09 12:36:36 +0530

    code chnges

--- FailureDetections/terraform/dashboards.tf
+++ FailureDetections/terraform/dashboards.tf
@@ -16,3 +16,18 @@ resource "azurerm_resource_group_template_deployment" "dashboards" {
   parameters_content = jsonencode({})
 }
 
+
+resource "azurerm_dashboard" "dashboards" {
+  for_each = { for f in local.dashboard_files : basename(f) => f }
+
+  name                = "${var.environment}-dashboard-${replace(each.key, ".json", "")}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  dashboard_properties = file(abspath("${path.module}/../Dashboard/${each.value}"))
+}
+
+output "dashboards_deployed" {
+  value = keys(azurerm_dashboard.dashboards)
+}
+
